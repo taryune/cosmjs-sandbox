d73a03a94f1bc58fe316d9461e65c293ee4d5770
taryune <taryunelab@gmail.com>
2023-01-06 13:38:03 +0100

    get balance

--- experiment.ts
+++ experiment.ts
@@ -1,5 +1,14 @@
+import {StargateClient} from "@cosmjs/stargate"
+
+const rpc = "rpc.sentry-01.theta-testnet.polypore.xyz:26657"
+
+const aliceAddress = "cosmos1g0dje5vu62fsg6vmltm9vcwujav9a0djjghq7u"
+
 const runAll = async (): Promise<void> => {
-  console.log("TODO")
+  const client = await StargateClient.connect(rpc)
+  console.log("With client, chain id: ", await client.getChainId(), "height: ", await client.getHeight())
+  console.log("Alice balances: ", await client.getAllBalances(aliceAddress))
+
 }
 
 runAll()
